69e469d0d0b142f3b5ff5a621acb466982750d2d
TBalo <tbalog426@gmail.com>
2024-09-22 19:31:34 +0100

    added authentication functions - login, signup, forgot password to todolist service

--- src/app/shared/todo-list.service.ts
+++ src/app/shared/todo-list.service.ts
@@ -1,51 +1,66 @@
 import { Injectable } from '@angular/core';
-import { TodoList } from './todo-list.model';
+import { TodoList } from './todo-list.model'; 
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
+  private userId: number = Number(localStorage.getItem('userId')); 
 
-  constructor(private http:HttpClient) {}
+  readonly baseURL = `http://localhost:5093/api/TodoList`;
+  readonly baseURLgetAsc = `http://localhost:5093/api/TodoList/user/asc/`;
+  readonly baseURLgetDesc = `http://localhost:5093/api/TodoList/user/desc/`;
 
-  readonly baseURL = 'http://localhost:5093/api/TodoList'
-  readonly baseURLgetAsc = 'http://localhost:5093/api/TodoList/GetTodoListsAsc'
-  readonly baseURLgetDesc = 'http://localhost:5093/api/TodoList/GetTodoListsDesc'
+  formData: TodoList = new TodoList();
+  list: TodoList[] = [];
 
-  formData:TodoList = new TodoList();
-  list : TodoList[];
+  constructor(private http: HttpClient) {}
 
-  postTodoList(){
-    return this.http.post(this.baseURL, this.formData);
+  private initializeUserId(): void {
+    this.userId = Number(localStorage.getItem('userId')); 
+    if (!this.userId) {
+      console.error('No userId found in localStorage.');
+    }
   }
 
-  putTodoList(){
-    return this.http.put(`${this.baseURL}/${this.formData.listId}`, this.formData);
+  postTodoList(): Observable<TodoList> {
+    this.initializeUserId(); 
+    this.formData.userId = this.userId;
+    return this.http.post<TodoList>(this.baseURL, this.formData);
   }
 
-  deleteTodoList(id:number){
-    return this.http.delete(`${this.baseURL}/${id}`);
+  putTodoList(): Observable<void> {
+    this.initializeUserId(); 
+    return this.http.put<void>(`${this.baseURL}/${this.formData.listId}`, this.formData);
   }
- 
-	refreshList(){
-		this.http.get(this.baseURLgetAsc)
-		.toPromise()
-			.then(res => this.list = res as TodoList[]);
-		}
 
-    getListAsc(){
-      this.http.get(this.baseURLgetAsc)
-      .toPromise()
-        .then(res => this.list = res as TodoList[]);
-      }
+  deleteTodoList(id: number): Observable<void> {
+    this.initializeUserId(); 
+    return this.http.delete<void>(`${this.baseURL}/${id}`);
+  }
 
-      getListDesc(){
-        this.http.get(this.baseURLgetDesc)
-        .toPromise()
-          .then(res => this.list = res as TodoList[]);
-        }
-    
-  
+  refreshList(): void {
+    this.getListAsc().subscribe(res => this.list = res);
+  }
 
+  getListAsc(): Observable<TodoList[]> {
+    this.initializeUserId(); 
+    return this.userId ? this.http.get<TodoList[]>(`${this.baseURLgetAsc}${this.userId}`) : of([]);
+  }
+
+  getListDesc(): Observable<TodoList[]> {
+    this.initializeUserId(); 
+    return this.userId ? this.http.get<TodoList[]>(`${this.baseURLgetDesc}${this.userId}`) : of([]);
+  }
+
+  updateUserId(newUserId: number): void {
+    this.userId = newUserId;
+    localStorage.setItem('userId', newUserId.toString());
+  }
+
+  clearCache(): void {
+    this.list = []; 
+  }
 }
